02214810eed201a980bb5b45b7c7237d036bef78
HotQ <calmoaf@gmail.com>
2017-01-21 14:41:19 +0800

    the simplest addion between two Integer.

--- CxxPrimer/demo.cpp
+++ CxxPrimer/demo.cpp
@@ -9,16 +9,12 @@ using std::endl;
 
 int main()
 {
-	Integer z11("9");
-	Integer z12("-9");
-	Integer z21("-10");
-	Integer z3("999999");
-	
-	z11.print(); cout << endl;
-	z12.print(); cout << endl;
-	(z12.reset()).print(); cout << endl;
-	z21.print(); cout << endl;
-	z3.print();  cout << endl;
-	
+	Integer z1("0xffffffff"),z2("10000000000"),z3("123");
+
+	z1.print();                      cout << endl;	
+	z2.print();                      cout << endl;
+	Integer_add(z1, z2, z3).print(); cout << endl;
+	Integer_add(z3, z2, z3).print(); cout << endl;
+
 	return 0;
 }
